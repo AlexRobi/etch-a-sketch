10d7523b7ce00ea05b4108315109c8d3f0af85c3
Alexis Robillart <alexisrobillart@gmail.com>
2017-12-28 19:58:16 +0100

    Added error message for wrong input

--- script.js
+++ script.js
@@ -19,24 +19,33 @@ function createGrid(gridSize) {
 };
 
 document.getElementById("input-grid-size").addEventListener('change', resize);
+
 function resize() {
   if (this.value >= 1 && this.value <= 64) {
     document.getElementById('grid').parentNode.removeChild(grid);
     createGrid(this.value)
+    document.getElementById("toggle-grid").textContent = "Grid: OFF";
   } else {
-    alert("Error! Enter a number between 1 and 64.");
+    let errorMessage = document.createElement('div');
+    errorMessage.setAttribute("class", "container text-center bg-danger text-white");
+    document.getElementById('error-container').appendChild(errorMessage);
+    errorMessage.textContent = "Error! Please enter a number between 1 and 62."
+    setTimeout(function() {
+      errorMessage.textContent = ""
+    }, 3000);
   };
 };
 
 document.getElementById("clear-grid").addEventListener('click', clearGrid);
+
 function clearGrid() {
-  const cells = document.querySelectorAll(`div[class="cell"]`);
+  let cells = document.querySelectorAll(`div.cell`);
   cells.forEach(cell => cell.style.background = 'white');
   containerGrid = document.getElementById('grid-container')
   containerGrid.classList.add("shake");
-  setTimeout(function(){
-      containerGrid.classList.remove("shake");
-    }, 500);
+  setTimeout(function() {
+    containerGrid.classList.remove("shake");
+  }, 500);
 };
 
 function randomColor() {
@@ -47,29 +56,44 @@ function randomColor() {
 }
 
 document.getElementById("color-trail").addEventListener('click', colorTrail);
+
 function colorTrail() {
-  const cells = document.querySelectorAll(`div[class="cell"]`);
+  let cells = document.querySelectorAll(`div.cell`);
   cells.forEach(cell => cell.onmouseover = function() {
     this.style.background = randomColor();
   });
 };
 
 document.getElementById("black-trail").addEventListener('click', blackTrail);
+
 function blackTrail() {
-  const cells = document.querySelectorAll(`div[class="cell"]`);
+  let cells = document.querySelectorAll(`div.cell`);
   cells.forEach(cell => cell.onmouseover = function() {
     this.style.background = '#1C1C1C';
   });
 };
 
 document.getElementById("color-input").addEventListener('change', colorPicker);
+
 function colorPicker() {
-  const cells = document.querySelectorAll(`div[class="cell"]`);
+  let cells = document.querySelectorAll(`div.cell`);
   colorSelected = this.value;
   cells.forEach(cell => cell.onclick = function() {
     this.style.background = `${colorSelected}`;
   });
-  cells.forEach(cell => cell.onmouseover = function() {
-  });
+  cells.forEach(cell => cell.onmouseover = function() {});
 };
+
+document.getElementById("toggle-grid").addEventListener('click', toggleGrid);
+
+function toggleGrid() {
+  let cells = document.querySelectorAll(`div.cell`);
+  cells.forEach(cell => cell.classList.toggle("grid"));
+  if (document.querySelectorAll('div.grid').length === 0) {
+    document.getElementById("toggle-grid").textContent = "Grid: OFF";
+  } else {
+    document.getElementById("toggle-grid").textContent = "Grid: ON";
+  }
+};
+
 createGrid(16);
